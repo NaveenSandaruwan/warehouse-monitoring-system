8a9e60ee36fc617d3d94762659b88b16035d19c9
NaveenSandaruwan <157776142+NaveenSandaruwan@users.noreply.github.com>
2025-01-19 16:22:40 +0530

    create work api

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,7 @@ from app.apis.asset import asset  # Import the asset API Blueprint
 from app.apis.task_api import task_api
 from app.apis.location_api import location_api  # Import the location API Blueprint
 from app.apis.user_api import user_api
+from app.apis.worker import work_api
 app = Flask(__name__)
 
 # Register Blueprints
@@ -10,6 +11,8 @@ app.register_blueprint(asset)
 app.register_blueprint(task_api)
 app.register_blueprint(location_api)
 app.register_blueprint(user_api)
+app.register_blueprint(work_api)
+
 @app.route("/")
 def home():
     return jsonify({"message": "Warehouse Tracking Backend is running!"})
